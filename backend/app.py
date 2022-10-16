b80b487e6642fadc6508acd414756f54f43e1d05
Freeman Jiang <freeman.jiang.ca@gmail.com>
2022-10-15 22:44:45 -0700

    Working replicate AI with mask

--- backend/app.py
+++ backend/app.py
@@ -18,21 +18,16 @@ def hello_world():  # put application's code here
 @app.route('/process', methods=['POST'])
 def modify_image():
     body = request.json
-    mask = base64.b64decode(body["mask"][22:])
-
-    with tempfile.TemporaryFile() as fp:
-        fp.write(mask)
-        fp.seek(0)
-        res = model.predict(
-            prompt=body["prompt"],
-            init_image=body["init_image"],
-            prompt_strength=body["prompt_strength"],
-            mask=fp,
-            num_outputs=1
-        )
-        return {
-            "response": res
-        }
+    res = model.predict(
+        prompt=body["prompt"],
+        init_image=body["init_image"],
+        prompt_strength=body["prompt_strength"],
+        mask=body["mask"],
+        num_outputs=1
+    )
+    return {
+        "response": res
+    }
 
 
 if __name__ == '__main__':
