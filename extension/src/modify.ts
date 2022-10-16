a211aaf8c515b77ebf30b69f30723d5f2977167e
Freeman Jiang <freeman.jiang.ca@gmail.com>
2022-10-16 01:37:06 -0700

    Drawing is working but offset

--- extension/src/modify.ts
+++ extension/src/modify.ts
@@ -1,12 +1,16 @@
 const sourceImage = <HTMLImageElement>document.getElementById("sourceImage");
-const canvas = <HTMLCanvasElement>document.getElementById("canvas");
+const canvasElement = <HTMLCanvasElement>document.getElementById("canvas");
+const context = canvasElement.getContext("2d");
 const canvasContainer = <HTMLDivElement>(
   document.getElementById("canvasContainer")
 );
 
 const computeDimensions = () => {
-  const originalWidth = sourceImage.naturalWidth;
-  const originalHeight = sourceImage.naturalHeight;
+  const originalWidth = sourceImage.clientWidth;
+  const originalHeight = sourceImage.clientHeight;
+
+  console.log("originalWidth", originalWidth);
+  console.log("originalHeight", originalHeight);
 
   const aspect = originalWidth / originalHeight;
 
@@ -32,5 +36,63 @@ const { height, width } = computeDimensions();
 
 canvasContainer.style.width = `${width}px`;
 canvasContainer.style.height = `${height}px`;
-canvas.style.width = `${width}px`;
-canvas.style.height = `${height}px`;
+
+canvasElement.width = width;
+canvasElement.height = height;
+
+// TODO: New one
+
+const clearButton = <HTMLButtonElement>document.getElementById("clear");
+clearButton.onclick = () => {
+  context.clearRect(0, 0, canvasElement.width, canvasElement.height);
+};
+
+const exportButton = <HTMLButtonElement>document.getElementById("export");
+exportButton.onclick = () => {
+  const img = canvasElement.toDataURL("image/png");
+  console.log(img);
+};
+
+const sizeElement = <HTMLInputElement>document.querySelector("#sizeRange");
+let size = sizeElement.value;
+sizeElement.oninput = (e: any) => {
+  size = e.target.value;
+};
+
+// enabling drawing on the blank canvas
+drawOnImage();
+
+function drawOnImage(image = null) {
+  const clearElement = document.getElementById("clear");
+  clearElement.onclick = () => {
+    context.clearRect(0, 0, canvasElement.width, canvasElement.height);
+  };
+
+  let isDrawing;
+
+  canvasElement.onmousedown = (e) => {
+    console.log("onmousedown");
+    isDrawing = true;
+    context.beginPath();
+    context.lineWidth = parseInt(size);
+    context.lineJoin = "round";
+    context.lineCap = "round";
+    context.moveTo(e.clientX, e.clientY);
+  };
+
+  canvasElement.onmousemove = (e) => {
+    console.log("onmousemove");
+
+    if (isDrawing) {
+      context.lineTo(e.clientX, e.clientY);
+      context.stroke();
+    }
+  };
+
+  canvasElement.onmouseup = function () {
+    console.log("on mouseup");
+
+    isDrawing = false;
+    context.closePath();
+  };
+}
