59fc2bd45d32c7682db3b8160f4a6c3e0bc4c254
Jason Yuan <jason.yuan869@gmail.com>
2022-10-16 08:49:44 -0700

    Improve image saving logic

--- extension/src/background.ts
+++ extension/src/background.ts
@@ -1,40 +1,41 @@
 let popupOpen: boolean = false;
 
 function getUrl(info) {
-    chrome.storage.local.get(function(storage) {
-        const urlList = storage.urlList;
-        if (urlList !== undefined) {
-            urlList.push(info.srcUrl);
-        }
+  chrome.storage.local.get("urlList", function ({urlList}) {
+    if (urlList !== undefined) {
+      urlList.push(info.srcUrl);
+    }
 
-        chrome.storage.local.set({urlList: storage.urlList || [info.srcUrl]});
-        if (popupOpen) {
+    chrome.storage.local.set({urlList: urlList || [info.srcUrl]})
+        .then(r => {
+          if (popupOpen) {
             chrome.runtime.sendMessage({msg: "refresh"});
-        }
-    });
+          }
+        });
+  });
 }
 
 chrome.runtime.onInstalled.addListener(() => {
-    chrome.contextMenus.create({
-        "id": "addNewImage",
-        "title": "Add image to Imaginate",
-        "contexts": ["image"],
-    });
-    chrome.contextMenus.onClicked.addListener(getUrl);
+  chrome.contextMenus.create({
+    "id": "addNewImage",
+    "title": "Add image to Imaginate",
+    "contexts": ["image"],
+  });
+  chrome.contextMenus.onClicked.addListener(getUrl);
 });
 
- chrome.runtime.onMessage.addListener(
-    function(request) {
-        if (request.msg === "loaded") {
-            popupOpen = true;
-        }
+chrome.runtime.onMessage.addListener(
+    function (request) {
+      if (request.msg === "loaded") {
+        popupOpen = true;
+      }
     }
- );
+);
 
-chrome.runtime.onConnect.addListener(function(port) {
-    if (port.name === "popup") {
-        port.onDisconnect.addListener(function() {
-           popupOpen = false;
-        });
-    }
+chrome.runtime.onConnect.addListener(function (port) {
+  if (port.name === "popup") {
+    port.onDisconnect.addListener(function () {
+      popupOpen = false;
+    });
+  }
 });
