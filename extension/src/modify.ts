50722d3d094ba1a88783c882f1f258d044f691f8
Jason Yuan <jason.yuan869@gmail.com>
2022-10-15 23:55:04 -0700

    More canvas formatting

--- extension/src/modify.ts
+++ extension/src/modify.ts
@@ -1,7 +1,26 @@
-const width = document.getElementById("sourceImage").clientWidth;
-const height = document.getElementById("sourceImage").clientHeight;
-console.log("width: " + width + " height: " + height);
-
+const sourceImage = document.getElementById("sourceImage");
 const canvas = <HTMLCanvasElement> document.getElementById("canvas");
-canvas.width = width;
-canvas.height = height;
+const canvasContainer = document.getElementById("canvasContainer");
+
+function updateDimensions() {
+  const width = sourceImage.clientWidth;
+  const height = sourceImage.clientHeight;
+  console.log("width: " + width + " height: " + height);
+
+  const aspect = width / height;
+
+  if (width > 1024) {
+    canvas.width = 1024;
+    canvas.height = 1024 / aspect;
+  }
+  if (height > 768) {
+    canvas.height = 768;
+    canvas.width = 768 * aspect;
+  }
+  canvasContainer.style.aspectRatio = aspect.toString();
+
+  canvas.width = width;
+  canvas.height = height;
+}
+updateDimensions();
+sourceImage.addEventListener("resize", updateDimensions);
